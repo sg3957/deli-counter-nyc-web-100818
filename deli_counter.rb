85e3ac9205651865ac37d87fe77308bfdedc5b6e
Sydney Grant <sg3957@nyu.edu>
2018-09-30 00:34:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 def take_a_number(katz_deli, name)
   if katz_deli.size == 0 
     katz_deli.push(name)
-    puts "Welcome, #{name}. You are number 1 in line."
+    "Welcome, #{name}. You are number 1 in line."
   else
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.size+1} in line."
