c12df4145c6a9f95367a89e7c9a9b62047f7afb9
Sydney Grant <sg3957@nyu.edu>
2018-09-30 00:34:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else
+    counter = 0
+    puts "The line is currently: "
+    until counter == arr.size
+     puts "#{counter+1}. #{katz_deli[counter]} "
+     counter += 1
+    end
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.size == 0 
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.size+1} in line."
+  end
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0 
+    puts "There is nobody waiting to be served!."
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
