a6f8496eb1a34e0ec47ebffcab974475a62e0a91
Sydney Grant <sg3957@nyu.edu>
2018-09-30 00:41:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   else
     counter = 0
     sentence = []
-    sentence << "The line is currently: "
+    sentence << "The line is currently:"
     until counter == katz_deli.size
      sentence << "#{counter+1}. #{katz_deli[counter]}"
      counter += 1
