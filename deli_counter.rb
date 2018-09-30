304f040cc3922b8199b7890460a5bd32d6951bfb
Sydney Grant <sg3957@nyu.edu>
2018-09-30 00:36:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,12 +6,13 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     counter = 0
-    puts "The line is currently: "
+    line_output = "The line is currently: "
     until counter == arr.size
-     puts "#{counter+1}. #{katz_deli[counter]} "
+     line_output + "#{counter+1}. #{katz_deli[counter]} "
      counter += 1
     end
   end
+  line_output
 end
 
 def take_a_number(katz_deli, name)
